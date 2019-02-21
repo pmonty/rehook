7062284711c56b21e3e7e55538b925a6a7349e03
paul.montagna <paul.montagna@gmail.com.au>
2019-02-21 17:05:36 +1100

    removed hook versions and unnecessary files

--- src/Index.tsx
+++ src/Index.tsx
@@ -5,15 +5,28 @@ import * as ReactDOM from "react-dom";
 import Cart from "./containers/Cart";
 import Products from "./containers/Products";
 
-function App() {
-  let [item, setItem] = useState({});
+class App extends React.Component<any, any> {
+  constructor(props: any) {
+    super(props);
 
-  return (
-    <>
-      <Products setItemCallback={setItem} />
-      <Cart cartItem={item} />
-    </>
-  );
+    this.state = {
+      item: {}
+    };
+  }
+
+  handleItemChange = (item: any) => {
+    this.setState({ item });
+  };
+
+  render() {
+    const { item } = this.state;
+    return (
+      <>
+        <Products setItemCallback={this.handleItemChange} />
+        <Cart cartItem={item} />
+      </>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById("example"));
