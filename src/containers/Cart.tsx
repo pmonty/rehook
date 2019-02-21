0dc410d4c3fa485ce3017afd8ffa3c726780aecc
paul.montagna <paul.montagna@gmail.com.au>
2019-02-21 16:43:42 +1100

    using hooks and functions

--- src/containers/Cart.tsx
+++ src/containers/Cart.tsx
@@ -20,32 +20,3 @@ export default function Cart(props: { cartItem: any }) {
     </div>
   );
 }
-
-// export default class Cart extends React.Component<any, any> {
-//   constructor(props: any) {
-//     super(props);
-//     this.state = {
-//       items: []
-//     };
-//   }
-
-//   componentDidUpdate(prevProps: any) {
-//     if (this.props.cartItem !== prevProps.cartItem)
-//       this.setState({ items: [...this.state.items, this.props.cartItem] });
-//   }
-
-//   render() {
-//     const { items } = this.state;
-//     return (
-//       <div>
-//         <ul>
-//           {items.map((item: any, i: number) => (
-//             <li key={item.price + Math.random() * i}>
-//               {item.name} - ${item.price}
-//             </li>
-//           ))}
-//         </ul>
-//       </div>
-//     );
-//   }
-// }
